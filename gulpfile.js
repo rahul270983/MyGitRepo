4260ff6464e49e2dc4d57b40fb78181c2e2e2fb4
rahul270983 <rahul.sinha@magicsw.com>
2016-04-07 15:11:13 +0530

    Files added
    New files added

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+// include gulp
+var gulp = require('gulp'); 
+// include plug-ins
+var jshint = require('gulp-jshint');
+var minifyHTML = require('gulp-minify-html');
+var changed = require('gulp-changed');
+var imagemin = require('gulp-imagemin');
+var concat = require('gulp-concat');
+var stripDebug = require('gulp-strip-debug');
+var uglify = require('gulp-uglify');
+var autoprefix = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
+
+// JS hint task
+gulp.task('jshint', function() {
+  gulp.src('./src/scripts/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+// minify new images
+gulp.task('imagemin', function() {
+  var imgSrc = './src/images/**/*',
+      imgDst = './build/images';
+  gulp.src(imgSrc)
+    .pipe(changed(imgDst))
+    .pipe(imagemin())
+    .pipe(gulp.dest(imgDst));
+});
+// JS concat, strip debugging and minify
+gulp.task('scripts', function() {
+  gulp.src(['./src/scripts/*.js'])
+    .pipe(concat('script.js'))
+    .pipe(stripDebug())
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/scripts/'));
+});
+
+
+// CSS concat, auto-prefix and minify
+gulp.task('styles', function() {
+  gulp.src(['./src/css/*.css'])
+    .pipe(concat('styles.css'))
+    .pipe(autoprefix('last 2 versions'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./build/css/'));
+});
+
+// minify new or changed HTML pages
+gulp.task('htmlpage', function() {
+  var htmlSrc = './src/*.html',
+      htmlDst = './build';
+
+  gulp.src(htmlSrc)
+    .pipe(changed(htmlDst))
+    .pipe(minifyHTML())
+    .pipe(gulp.dest(htmlDst));
+});
+
+// default gulp task
+gulp.task('default', ['imagemin','htmlpage','scripts', 'jshint','styles'], function() {
+});
